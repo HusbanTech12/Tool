796c39f1b002631a7678cbb69f8a045f0e8181a0
HusbanTech12 <husbantech08@gmail.com>
2025-07-17 23:54:57 +0500

    Add Funtion_tool.py

---
+++ function_tool.py
@@ -0,0 +1,33 @@
+from agents import Agent , Runner , function_tool
+from main import config
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+weather_api_key = os.getenv("WEATHER_API_KEY")
+
+
+@function_tool
+def weather(city : str) -> str:
+    result = requests.get(f" http://api.weatherapi.com/v1/current.json?key={weather_api_key}&q{city}")
+    
+    data = result.json()
+    
+    
+    return f"The current weather in {city} is {data['current']['temp_c']} c"
+
+agent = Agent(
+    name ='Helpful Assistant',
+    instructions='You are a Helpful Assistant',
+    tools=[weather]
+    
+)
+
+Run = Runner.run_sync(
+    agent,
+    'what is USD to PKR today',
+    run_config=config
+)
+
+print(Run.final_output)
